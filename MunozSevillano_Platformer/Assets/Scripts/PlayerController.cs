9ba9e96ccff39962cf093cec7d615b65cadf08e9
SrSiestas15 <148352083+SrSiestas15@users.noreply.github.com>
2024-11-20 20:25:16 -0500

    Finished all three tasks - Journal 10

--- MunozSevillano_Platformer/Assets/Scripts/PlayerController.cs
+++ MunozSevillano_Platformer/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float speed;
+    Vector2 playerInput;
+  
+    FacingDirection lastDirection;
+
+    bool groundBellow;
+    public LayerMask groundLayer;
+
     public enum FacingDirection
     {
         left, right
@@ -12,7 +21,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,26 +29,46 @@ public class PlayerController : MonoBehaviour
     {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+        playerInput = new Vector2(Input.GetAxis("Horizontal"), 0f);
+    }
+
+    private void FixedUpdate()
+    {
+        groundBellow = Physics2D.Linecast(transform.position, transform.position + Vector3.down, groundLayer);
+        //Debug.DrawLine(transform.position, transform.position + Vector3.down);
         MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-
+        rb.AddForce(playerInput * speed * 1000 * Time.fixedDeltaTime);
     }
 
     public bool IsWalking()
     {
-        return false;
+        if (Mathf.Abs(playerInput.x) > 0)
+        {
+            return true;
+        } else return false;
     }
     public bool IsGrounded()
     {
-        return true;
+        //Debug.Log("touching ground");
+        return groundBellow;
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        if (playerInput.x > 0)
+        {
+            lastDirection = FacingDirection.right;
+            return FacingDirection.right;
+        }
+        else if (playerInput.x < 0)
+        {
+            lastDirection = FacingDirection.left;
+            return FacingDirection.left;
+        }
+        else return lastDirection;
     }
 }
